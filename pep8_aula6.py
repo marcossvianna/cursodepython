962932f6788a4cc477d998c043be6167eb861dbb
Marcos Santana Viana <marcossvianna@hotmail.com>
2024-07-08 20:42:17 -0300

    Atualizando o PEP8

---
+++ pep8_aula6.py
@@ -0,0 +1,21 @@
+"""
+PEP8 - Python Enchancement Proposal
+
+São propostas de melhorias para a linguagem Python
+
+The Zen of Python
+
+import this
+
+A ideia da PEP8 e que possamos escrever codigos Python de forma Pythonica
+
+
+[1] - Utilize Camel Case para nomes de classes
+
+"""
+
+class Calculadora:
+    pass
+
+class CalculadoraCientifica:
+    pass
